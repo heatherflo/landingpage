175d010aa7c3765a5176d3f2d62837dec765d424
Heather Floyd <heatherflo@gmail.com>
2024-02-28 14:30:21 -0700

    Landing Page start

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class=" portfolio">
+    <div class="col-12">
+
+
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  setup() {
+    return {}
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.portfolio {
+  height: 80vh;
+  background-color: #43c5b8;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid">
+    <div class="col-12">
+
+      <Portfolio />
+    </div>
+  </div>
+</template>
+
+<script>
+import Portfolio from '../components/Portfolio.vue';
+
+export default {
+  setup() {
+    return {};
+  },
+  components: { Portfolio }
+}
+</script>
+
+<style scoped lang="scss"></style>
