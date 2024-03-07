fc55326e346927d8a099d3a68aae6e64fe8d32f5
Heather Floyd <heatherflo@gmail.com>
2024-03-07 11:12:46 -0700

    slide show present

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <router-view />
+  </main>
+  
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  },
+  components: { Navbar }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root {
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+
+
+</style>
